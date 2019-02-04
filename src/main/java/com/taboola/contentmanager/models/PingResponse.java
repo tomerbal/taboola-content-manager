d7ce04bfbd61eaa18fc34ffc613a8d2a01e8ac4d
tomer <tomerbalan@gmail.com>
2019-02-04 22:35:41 +0200

    Start

---
+++ src/main/java/com/taboola/contentmanager/models/PingResponse.java
@@ -0,0 +1,8 @@
+package com.taboola.contentmanager.models;
+
+import lombok.Data;
+
+@Data
+public class PingResponse {
+    private final String status;
+}
---
+++ src/test/java/com/taboola/contentmanager/ContentManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.taboola.contentmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContentManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
